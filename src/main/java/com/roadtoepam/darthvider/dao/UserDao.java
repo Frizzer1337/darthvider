6eb177e35405e84eb43c074d3078395c641c26fe
Frizzer1337 <eo.ursuz@gmail.com>
2021-08-19 17:45:02 +0300

    Update UserDao.java

--- src/main/java/com/roadtoepam/darthvider/dao/UserDao.java
+++ src/main/java/com/roadtoepam/darthvider/dao/UserDao.java
@@ -12,7 +12,7 @@ public interface UserDao {
 	   * Get all users.
 	   *
 	   * @return all the customers as a list.
-	   * @throws Exception if any error occurs.
+	   * @throws DaoException if any error occurs.
 	   */
 	  List<User> findAll() throws DaoException;
 
@@ -22,34 +22,34 @@ public interface UserDao {
 	   * @param id unique identifier of the user.
 	   * @return an optional with customer if a customer with unique identifier <code>id</code> exists,
 	   *     empty optional otherwise.
-	   * @throws Exception if any error occurs.
+	   * @throws DaoException if any error occurs.
 	   */
 	  Optional<User> findById(int id) throws DaoException;
 
 	  /**
 	   * Add a customer.
 	   *
-	   * @param customer the customer to be added.
-	   * @return true if customer is successfully added, false if customer already exists.
+	   * @param user to be added.
+	   * @return true if user is successfully added, false if customer already exists.
 	   * @throws Exception if any error occurs.
 	   */
 	  boolean add(User customer, String password) throws DaoException;
 
 	  /**
-	   * Update a customer.
+	   * Update a user.
 	   *
-	   * @param customer the customer to be updated.
-	   * @return true if customer exists and is successfully updated, false otherwise.
+	   * @param user to be updated.
+	   * @return true if user exists and is successfully updated, false otherwise.
 	   * @throws Exception if any error occurs.
 	   */
 	  boolean update(User customer) throws DaoException;
 
 	  /**
-	   * Delete a customer.
+	   * Delete a user.
 	   *
-	   * @param customer the customer to be deleted.
-	   * @return true if customer exists and is successfully deleted, false otherwise.
-	   * @throws Exception if any error occurs.
+	   * @param user to be deleted.
+	   * @return true if user exists and is successfully deleted, false otherwise.
+	   * @throws DaoException if any error occurs.
 	   */
 	  boolean delete(User customer) throws DaoException;
 	}
