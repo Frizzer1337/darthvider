cd625af42adebb015e18f65d813ca30044486146
Frizzer1337 <81990525+Frizzer1337@users.noreply.github.com>
2022-02-15 18:43:13 +0300

    Delete src/main/webapp/jsp/lk directory

--- src/main/webapp/jsp/lk/js/src/js/main.js
+++
@@ -1,1 +0,0 @@
-$(".reg_btn").click(function(){$("#login").hide(),$("#reg").show(),$(".reg_btn").addClass("active_bttn"),$(".login_btn").removeClass("active_bttn")}),$(".login_btn").click(function(){$("#login").show(),$("#reg").hide(),$(".login_btn").addClass("active_bttn"),$(".reg_btn").removeClass("active_bttn")});
